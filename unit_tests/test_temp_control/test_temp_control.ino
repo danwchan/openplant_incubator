cf3fc7984ce51d6ee6593898f86319bc4a7ef7d5
danwchan <danwchan@gmail.com>
2019-01-15 19:55:44 -0500

    the PID doesn't compute...

--- unit_tests/test_temp_control/test_temp_control.ino
+++ unit_tests/test_temp_control/test_temp_control.ino
@@ -33,6 +33,7 @@ class Tempsensor: public Adafruit_Si7021 {
 
   void Update() {
     unsigned long currentMillis = millis();
+//    Serial.print("Current millis() = "); Serial.println(currentMillis);
 
     if((currentMillis - lastUpdate) >= (readInterval * 1000)) {
       lastUpdate = millis();
@@ -70,16 +71,20 @@ class Fan {
     OCR2B = 30;       // set the PWM duty cycle, this is the lower limit of speed
   }
 
+  void Configure() {
+    pinMode(outPin, OUTPUT);
+  }
   void set_speed(double output) {
     byteControl = byte(output);                     // ideally will be the PWM output of the PID controller
-    OCR2B = map(byteControl, 0, 255, 30, 79);     // fan speed will thus be proportional to the PID output
+    OCR2B = map(byteControl, 0, 255, 30, 79);       // fan speed will thus be proportional to the PID output
   }
 };
 
 class Peltier: public PID {
   private:
     byte outPin;                // the pin which the peltier control is attached to
-    int readInterval;  // the time in milliseconds between readings
+    byte readInterval;  // the time in milliseconds between readings
+    unsigned long lastUpdate;
     const double Kp = 150;
     const double Ki = 20;
     const double Kd = 1;
@@ -90,33 +95,63 @@ class Peltier: public PID {
     double setPoint;
     double gap;
 
-  Peltier(byte pin, int milliseconds, double newSetPoint = 25) : PID(&pidInput, &pidOutput, &setPoint, Kp, Ki, Kd, REVERSE) {
+  Peltier(byte pin, byte seconds, double newSetPoint = 25) : PID(&pidInput, &pidOutput, &setPoint, Kp, Ki, Kd, REVERSE) {
     outPin = pin;
-    readInterval = milliseconds;
+//    pidInput = 100;
+//    pidOutput = 100;
+    readInterval = seconds;
     setPoint = newSetPoint;
-    SetSampleTime(readInterval);
+    lastUpdate = millis();
   }
+
+//  void Configure() {
+//    SetSampleTime(readInterval);
+//  }
 
   void set_peltier(double newSetPoint) {
     setPoint = newSetPoint;
   }
 
   void Update(Tempsensor sensor) {
-    pidInput = sensor.temp;
-    gap = pidInput - setPoint;
-    if (gap > 1) {
-      pidOutput = 255;
+    unsigned long currentMillis = millis();
+//    Serial.print("inside update"); Serial.println();
+
+    if((currentMillis - lastUpdate) >= (readInterval * 1000)) {
+      lastUpdate = millis();
+      pidInput = sensor.temp;
+      gap = pidInput - setPoint;
+//      Serial.print("update triggered"); Serial.println();
+//      Serial.print("gap = "); Serial.println(gap);
+      
+      if (gap > 1) {
+        pidOutput = 255;
+      }
+      else {
+        Compute();
+//        Serial.print("PWM output = "); Serial.println(pidOutput);       
+      }
+      
+      analogWrite(outPin, pidOutput);
     }
-    else {
-      Compute();
-      analogWrite(outPin, pidOutput);      
+  }
+
+  void testprint() {
+    unsigned long currentMillis = millis();
+
+    if((currentMillis - lastUpdate) >= (readInterval * 1000)) {
+      Serial.print("Current millis() = "); Serial.println(currentMillis);
+      Serial.print("Millis of last poll = "); Serial.println(lastUpdate);
+      Serial.print("gap = "); Serial.println(gap);
+      Serial.print("Temp *C = "); Serial.println(pidInput);
+      Serial.print("PID output = "); Serial.println(pidOutput);
+      Serial.println();
     }
   }
 };
 
-Fan testfan = Fan(3);
-Peltier testpeltier = Peltier(5, 2000, 23);
-Tempsensor testsensor = Tempsensor(2); // initialize the sensor
+Fan testfan = Fan(3);                        // initialize the fan
+Peltier testpeltier = Peltier(5, 2, 23);     // initialize the peltier
+Tempsensor testsensor = Tempsensor(2);       // initialize the sensor
 
 
 /**************************************************************************/
@@ -145,7 +180,32 @@ void setup() {
   }
   
   /*turn the PID on*/
+//  testpeltier.Configure();
   testpeltier.SetMode(AUTOMATIC); 
+
+  testfan.Configure();
+
+  /*test the hardware*/
+  Serial.print("touch peltier, keep the fan in front of you test begins in"); Serial.println();
+  Serial.print("3"); Serial.println();
+  delay(1000);
+  Serial.print("2"); Serial.println();
+  delay(1000);
+  Serial.print("1"); Serial.println();
+  delay(1000);
+  Serial.print("fan low and peltier on"); Serial.println();
+  analogWrite(5, 255);
+  delay(5000);
+  Serial.print("fan low and peltier off"); Serial.println();
+  analogWrite(5, 0);
+  delay(5000);
+  Serial.print("fan high and peltier on"); Serial.println();
+  testfan.set_speed(255);
+  analogWrite(5, 255);
+  delay(5000);
+  testfan.set_speed(0);
+  analogWrite(5, 0);
+  Serial.print("fan test on pin 3 and peltier on pin 5 complete"); Serial.println();
 }
   
 /**************************************************************************/
@@ -156,7 +216,9 @@ void setup() {
 
 void loop() {
   testsensor.Update();
-  testsensor.testprint();
-//  testpeltier.Update(testsensor);
-//  testfan.set_speed(testpeltier.pidOutput);
+//  testsensor.testprint();
+//  Serial.print("tick tick tick"); Serial.println();
+  testpeltier.testprint();
+  testpeltier.Update(testsensor);
+  testfan.set_speed(testpeltier.pidOutput);
 }
